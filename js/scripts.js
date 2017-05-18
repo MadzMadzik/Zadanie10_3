06890765565a4d80de98ba1e61c62e4eb7f748c4
MadzMadzik <mag.sobieszuk@gmail.com>
2017-05-18 19:32:01 +0200

    change code

--- js/scripts.js
+++ js/scripts.js
@@ -8,5 +8,5 @@ function moveFirstSlide(){
   var firstItem = carouselList.find('li:first');
   var lastItem = carouselList.find('li:last');
   lastItem.after(firstItem);
-  lastItem.after(firstItem)
+  carouselList.css({'marginLeft':0});
 }
